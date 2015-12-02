b48be118bf3fb29e1d8e6007f40cebce13ed1ad9
Cassiszuoan <jerry831210@gmail.com>
2015-12-02 15:41:30 +0800

    Initial Commit

---
+++ convertCalculator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  convertCalculator
+//
+//  Created by JerryMAc on 2015/12/2.
+//  Copyright © 2015年 JerryMAc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
